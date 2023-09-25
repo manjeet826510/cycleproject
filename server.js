2264579da52cc1f32d9dd6af6f09422d117bd643
manjeet826510 <manjeet826510@gmail.com>
2023-09-26 01:30:44 +0530

    4th commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRoutes.js";
 import path from "path";
 import cors from "cors";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use("/api/users", userRouter);
 
 //serving the frontend
 // const __dirname = path.resolve();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
